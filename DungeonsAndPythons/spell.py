283dcebd61f6b5fc4e3a111cd88bd303067cf7bd
AntonNaumov <antonnaumov1997@gmail.com>
2018-04-24 00:43:30 +0300

    Add spell.py

---
+++ DungeonsAndPythons/spell.py
@@ -0,0 +1,35 @@
+class Spell:
+    def __init__(self, *, name, damage, mana_cost, cast_range):
+        self._name = name
+        self._damage = damage
+        self._mana_cost = mana_cost
+        self._cast_range = cast_range
+
+    @property
+    def get_name(self):
+        return self._name
+
+    @property
+    def get_damage(self):
+        return self._damage
+
+    @property
+    def get_mana_cost(self):
+        return self._mana_cost
+
+    @property
+    def get_cast_range(self):
+        return self._cast_range
+
+    def __str__(self):
+        return f'Spell {self._name}: damage - {self._damage}, '\
+               f'mana_cost - {self._mana_cost}, cast_range - {self._cast_range}'
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        return self._name == other._name and\
+               self._damage == other._damage and\
+               self._mana_cost == other._mana_cost and\
+               self._cast_range == other._cast_range
