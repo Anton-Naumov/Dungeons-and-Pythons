ea03eb7e7619640d00817b215714ce4b3db20e7d
AntonNaumov <antonnaumov1997@gmail.com>
2018-04-24 01:32:12 +0300

    Fixed enemy tests

--- DungeonsAndPythons/enemy.py
+++ DungeonsAndPythons/enemy.py
@@ -15,14 +15,14 @@ class Enemy(PlayerMixin):
         self.__pos = pos
 
     def can_cast(self):
-        return self._mana >= self._spell.get_mana_cost
+        return self._mana >= self._spell.mana_cost
 
     def attack(self, *, by):
         if by == 'weapon' and self._weapon is not None:
             return self.__damage + self._weapon.get_damage
         elif by == 'spell' and self._spell is not None:
             if self.can_cast():
-                self._mana -= self._spell.get_mana_cost
+                self._mana -= self._spell._mana_cost
                 return self.__damage + self._spell.get_damage
             else:
                 raise Exception
@@ -30,5 +30,5 @@ class Enemy(PlayerMixin):
             return self.__damage
 
     def take_mana(self):
-        print('You cannot regenerate mana!')
+        # print('You cannot regenerate mana!')
         return False
